1578fcc6a26f52f85817bf57774a3a710d22f0bc
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-08-01 09:28:46 +0200

    Base ghost system

---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerState
+    {
+        private Vector3 _position;
+        public Vector3 Position{get{return _position;} set{_position=value;}}
+    }
+}
