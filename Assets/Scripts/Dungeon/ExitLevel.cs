a5c79f60039f15518f3367b372cf9928bb2811dd
Evan DUBOIS <72149631+Evan-DUBOIS-02@users.noreply.github.com>
2025-08-03 12:33:19 +0200

    main menu, audio integration

---
+++ Assets/Scripts/Dungeon/ExitLevel.cs
@@ -0,0 +1,17 @@
+﻿using Game;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Dungeon
+{
+    public class ExitLevel: MonoBehaviour
+    {
+        private void OnTriggerEnter2D (Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                GameManager.Instance.EndLevel();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenuUI: MonoBehaviour
+    {
+        [SerializeField] private GameObject _mainMenuElement;
+        [SerializeField] private GameObject _creditsMenuElement;
+        [SerializeField] private string _nextLevelName;
+
+        public void OnReturnCreditClicked()
+        {
+            Debug.Log("OnReturnCreditClicked");
+            _creditsMenuElement.SetActive(false);
+            _mainMenuElement.SetActive(true);
+        }
+        
+        public void OnCreditClicked()
+        {
+            Debug.Log("OnCreditClicked");
+            _creditsMenuElement.SetActive(true);
+            _mainMenuElement.SetActive(false);
+        }
+
+        public void OnPlayClicked()
+        {
+            Debug.Log("Play");
+            SceneManager.LoadScene(_nextLevelName);
+        }
+
+        public void OnExitClicked()
+        {
+            Debug.Log("Exit");
+            Application.Quit();
+        }
+    }
+}
